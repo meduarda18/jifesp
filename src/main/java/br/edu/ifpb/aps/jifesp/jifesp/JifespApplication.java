c509104fcc83292ea1b680bca224e3a758ace8fe
meduarda18 <mariaeduardac604@gmail.com>
2025-02-07 09:08:25 -0300

    Inicialização do projeto com spring boot

---
+++ src/main/java/br/edu/ifpb/aps/jifesp/jifesp/JifespApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.aps.jifesp.jifesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JifespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JifespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/aps/jifesp/jifesp/JifespApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.aps.jifesp.jifesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JifespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
