ed4d808928ec26d9b2ba27292e233a28b5d73921
meduarda18 <mariaeduardac604@gmail.com>
2025-02-18 15:33:29 -0300

    Removi os metodos desnecessários do MonitorEntity

--- src/main/java/br/edu/ifpb/aps/jifesp/entity/MonitorEntity.java
+++ src/main/java/br/edu/ifpb/aps/jifesp/entity/MonitorEntity.java
@@ -10,7 +10,4 @@ public class MonitorEntity extends UsuarioEntity {
     public MonitorEntity(String nome, int matricula) {
         super(nome, matricula);
     }
-
-    void registrarJogo(){}
-    void registrarResultado(){}
 }
